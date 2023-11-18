4a1a0c54cd72377209e2a197e0dfab93f8409b48
magvemom <magvenom@bk.ru>
2023-11-18 15:21:26 +0300

    improve speed

--- src/routes/balance.controller.ts
+++ src/routes/balance.controller.ts
@@ -1,23 +1,32 @@
 import express from 'express';
 import { User } from '../models/user.model';
 import { Sequelize } from 'sequelize-typescript';
+import { sequelize } from '../configs/sequelize.config';
 
 export const balanceRouter = express.Router();
 
 balanceRouter.post('/change', async (req, res) => {
-	
-    const { amount, userId }: { amount: number; userId: number } = req.body;
-    const transaction = await User.sequelize.transaction();
+	const { amount, userId }: { amount: number; userId: number } = req.body;
+	const transaction = await User.sequelize.transaction();
 
 	try {
-		const user = await User.findOne({ where: { id: userId }, transaction });
-		if (!user) throw 'User not found!';
+		const [_, update] = await User.update(
+			{
+				balance: sequelize.literal(`balance+${amount}`)
+			},
+			{ where: { id: userId }, returning: ['balance'], transaction }
+		);
 
-		await user.update({ balance: user.balance + amount }, { transaction });
+		if(update.length===0)
+		throw 'User not found!';
+
+		if(update[0].balance<0)
+		throw 'Balance must be >=0!';
+
 		await transaction.commit();
-		res.status(200).send(`Balance: ${user.balance}`);
+		res.status(200).send(`Balance: ${update[0].balance}`);
 	} catch (err) {
 		await transaction.rollback();
-		res.status(400).send(typeof err === 'string' ? err : 'Balance must be >=0!');
+		res.status(400).send(err);
 	}
 });
